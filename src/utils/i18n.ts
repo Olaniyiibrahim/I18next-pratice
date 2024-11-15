c333d1d3ec84f9b5e071e2e636cff5a15c8ff9fb
ibrahim <olaniyiibrahim2006@gmail.com>
2024-11-15 11:27:17 +0100

    Rt

---
+++ src/utils/i18n.ts
@@ -0,0 +1,36 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+// the translations
+// (tip move them in a JSON file and import them,
+// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+const resources = {
+  en: {
+    translation: {
+        Welcome_one  : "Welcome to React and {{count}} react-i18next",
+        // Welcome_other : "Welcome to React and {{count}} react-i18next"
+    }  
+  },
+  fr: {
+    translation: {
+        Welcome_one : "Bienvenue à {{count}} React et react-i18next",
+        // Welcome_other : "Bienvenue à {{count}} React et react-i18next"
+    }
+  }
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    fallbackLng : "en",
+    lng: "fr", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
+    // if you're using a language detector, do not define the lng option
+
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
+
+  export default i18n;
